583af1ac9b605e95770bb9b276a8e71a56a4a878
bw4sz <benweinstein2010@gmail.com>
2016-03-09 17:03:07 -0800

    working on relative layout

--- GUI.py
+++ GUI.py
@@ -9,19 +9,18 @@ from kivy.uix.button import Button
 from kivy.uix.slider import Slider
 from kivy.uix.checkbox import CheckBox
 
-
 #For hyperlinks
 import webbrowser
         
 class MotionMeerkatApp(App):
     def build(self):
-        
+                
         #create overall layout
         b = BoxLayout(orientation='vertical',spacing=0)
         
         #MotionMeerkat
-        top = BoxLayout(orientation='vertical',spacing=-4,size_hint=(.2,1))
-        t1=Label(text="MotionMeerkat V2.0",font_size=15)
+        top = BoxLayout(orientation='vertical',spacing=-1,size_hint=(.25,1))
+        t1=Label(text="MotionMeerkat V2.0",font_size=12)
         t2=Button(text="Help",font_size=10)
         
         def help_site(instance):
@@ -57,14 +56,14 @@ class MotionMeerkatApp(App):
         
         mogbox = BoxLayout(orientation='horizontal',spacing=10)
         
-        moglabel=Label(text="How much background variation [eg. wind, waves, debris] do you expect in your video?",font_size=15)
+        moglabel=Label(text="How much background variation [eg. wind, waves, debris] do you expect in your video?",font_size=15,size_hint=(1,1))
         moglearn = Slider(min=0, max=5, value=3,size_hint=(.7,1),step=1)
         
         #define high
-        moglow=Label(text="No Movement",font_size=13,size_hint=(.15,1))
+        moglow=Label(text="No Movement",font_size=13.5,size_hint=(.15,1))
 
         #define low
-        moghigh=Label(text="Extreme movement",font_size=13,size_hint=(.15,1))
+        moghigh=Label(text="Extreme movement",font_size=13.5,size_hint=(.15,1))
         
         #Define output
         mogout=Label(font_size=20,size_hint=(.1,1))
@@ -89,14 +88,14 @@ class MotionMeerkatApp(App):
         #######################
         
         tolbox = BoxLayout(orientation='horizontal',spacing=10)
-        tollabel=Label(text="How quickly does your organism move?",font_size=15)
+        tollabel=Label(text="How quickly does your organism move?",font_size=15,size_hint=(1,0.6))
         tollearn = Slider(min=0, max=5, value=3,size_hint=(.7,1),step=1)
         
         #define high
-        tollow=Label(text="Slow",font_size=13,size_hint=(.15,1))
+        tollow=Label(text="Slow",font_size=13.5,size_hint=(.15,1))
 
         #define low
-        tolhigh=Label(text="Fast",font_size=13,size_hint=(.15,1))
+        tolhigh=Label(text="Fast",font_size=13.5,size_hint=(.15,1))
         
         #Define output
         tolout=Label(font_size=20,size_hint=(.1,1))
@@ -129,33 +128,46 @@ class MotionMeerkatApp(App):
         crop = CheckBox()
         crop.bind(active=on_checkbox_active)        
         
-        
         ####################
         #Minimum size
         ####################
         
-        #it can be either enter or draw
-        ms=BoxLayout()
-
-        #labels
-        drawl=Label(text="Enter minimum object size\n(% of the frame)",font_size=13,size_hint=(1,1))
-        checkl=Label(text="Or check box to draw your\nobject size on screen",font_size=13)
-        roil=Label(text="Would you like to crop region\nof motion detection?",font_size=13)
+        # OVerall Text Layout
+        ms=BoxLayout(size_hint=(1,.05))
         
-        mstext=TextInput(text="0.03",size_hint=(1,1))
+        #Overall Action Layout
+        draw_crop=BoxLayout(orientation='horizontal',pos_hint={'center_x':0.52})
+        
+        #label for layours
+        mins_layout_label=BoxLayout()
+        
+        #labels
+        drawl=Label(text="Minimum object size (% frame)",font_size=13)
+        checkl=Label(text="Draw your object size on screen",font_size=13)
+        roil=Label(text="Crop area of motion detection?",font_size=13)
+        
+        #Add to layout
+        mins_layout_label.add_widget(drawl)
+        mins_layout_label.add_widget(checkl)
+        
+        ms.add_widget(mins_layout_label)
+        ms.add_widget(roil)        
+
+        #Just a layout for the text box
+        mins_layout_action=BoxLayout()
+        
+        mstext=TextInput(text="0.03",size_hint=(.4,.4),pos_hint={'center_y':0.5})   
+        orlabel=Label(text="Or",font_size=11,size_hint=(0.3,1),pos=(100,100))
         
         draw = CheckBox()
         draw.bind(active=on_checkbox_active)  
         
-        ms.add_widget(drawl)
-        ms.add_widget(checkl)
-        ms.add_widget(roil)
-        
-        draw_crop=BoxLayout(orientation='horizontal')
-        
         #add to layout       
-        draw_crop.add_widget(mstext)
-        draw_crop.add_widget(draw)
+        mins_layout_action.add_widget(mstext)
+        mins_layout_action.add_widget(orlabel)
+        mins_layout_action.add_widget(draw)
+        
+        draw_crop.add_widget(mins_layout_action)
         draw_crop.add_widget(crop)
         
         #add to overall layout
@@ -163,7 +175,7 @@ class MotionMeerkatApp(App):
         b.add_widget(draw_crop)
         
         #Run!
-        t = Button(text='Run',font_size=40, height=200)
+        t = Button(text='Run',font_size=40,size_hint=(1,.7))
         b.add_widget(t)
         
         return b
